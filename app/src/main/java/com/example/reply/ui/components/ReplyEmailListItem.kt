4c9cd90a671c4e4601fc7f102f4b9b4d11263c5f
AliAkbar <aliakbarranjdoost@gmail.com>
2023-11-09 13:10:06 +0330

    Pathway 2 - Activity 4 - Part 6. Typography

--- app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
+++ app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
@@ -79,9 +79,11 @@ fun ReplyEmailListItem(
                 ) {
                     Text(
                         text = email.sender.firstName,
+                        style = MaterialTheme.typography.labelMedium
                     )
                     Text(
                         text = email.createdAt,
+                        style = MaterialTheme.typography.labelMedium
                     )
                 }
                 IconButton(
@@ -100,10 +102,12 @@ fun ReplyEmailListItem(
             Text(
                 text = email.subject,
                 modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
+                style = MaterialTheme.typography.titleLarge
             )
             Text(
                 text = email.body,
                 maxLines = 2,
+                style = MaterialTheme.typography.bodyLarge,
                 overflow = TextOverflow.Ellipsis
             )
         }
